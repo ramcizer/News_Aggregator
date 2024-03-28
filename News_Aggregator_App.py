e689f7bfa66f42b70848ddbe20e754b6d6d3b578
Ramis Cizer Kamil <ramcizer@gmail.com>
2024-03-28 00:16:05 +0000

    Update News_Aggregator_App.py

--- News_Aggregator_App.py
+++ News_Aggregator_App.py
@@ -324,10 +324,10 @@ cursor = conn.cursor()
 pio.templates.default = 'plotly'
 
 
-with st.spinner('Wait for it...Just getting together the most up-to-date WordCloud'):
-    fig, plt, representative_topics, orgs, people, GPEs, NORPs, products =  my_task(connection=conn, cursor=cursor)
-    time.sleep(5)
-st.success('Done!')
+# with st.spinner('Wait for it...Just getting together the most up-to-date WordCloud'):
+#     fig, plt, representative_topics, orgs, people, GPEs, NORPs, products =  my_task(connection=conn, cursor=cursor)
+#     time.sleep(5)
+# st.success('Done!')
 
 # fig, plt, representative_topics, orgs, people, GPEs, NORPs, products =  my_task(connection=conn, cursor=cursor)
 
