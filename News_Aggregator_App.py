2d67191eb2fce8d2566b06e8755b7070c07e5f41
Ramis Cizer Kamil <ramcizer@gmail.com>
2024-09-07 20:25:38 +0100

    Update News_Aggregator_App.py

--- News_Aggregator_App.py
+++ News_Aggregator_App.py
@@ -2,8 +2,8 @@ import psycopg2
 import re 
 import pandas as pd
 from datetime import datetime
-#import spacy
-import spacy_streamlit
+import spacy
+# import spacy_streamlit
 from spacytextblob.spacytextblob import SpacyTextBlob
 from bertopic import BERTopic
 import plotly.graph_objs as go
